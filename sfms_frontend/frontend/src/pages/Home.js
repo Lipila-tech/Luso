8103e5548287d4484fa6aa68cd1b7bb85798cc1b
Pita Sangwani Zyambo <mightypz2@gmail.com>
2022-05-10 14:39:18 +0200

    Frontend first commit

---
+++ sfms_frontend/frontend/src/pages/Home.js
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from 'reactstrap';
+import '../styles.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h2>Sign In</h2>
+        <Form className="form">
+          <FormGroup>
+            <Label for="exampleEmail">Username</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="example@example.com"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="examplePassword"
+              placeholder="********"
+            />
+          </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    </div>
+  );
+}
+}
+
+export default Home;
