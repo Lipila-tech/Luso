8455e2859e5efac305805c9b0b866e049f0233b5
Pita Sangwani Zyambo <mightypz2@gmail.com>
2022-05-30 00:21:49 +0200

    style landing page

--- sfms_frontend/frontend/src/components/Landing.js
+++ sfms_frontend/frontend/src/components/Landing.js
@@ -2,50 +2,109 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles.css';
 // Import medial
-import banner from "../assets/banner1.jpg"
-import PayGif from "../assets/payment.gif"
-import AdmGif from "../assets/admin.gif"
-import HisImg from "../assets/history.png"
+import banner from "../assets/banner2.jpg";
+import PayGif from "../assets/payment.gif";
+import AdmGif from "../assets/admin.gif";
+import HisImg from "../assets/history.jpg";
+
 
 const Landing = () => {
 
     return (
       <div>
-        <section>
-          <h2>A Student Fees Mangement System</h2>
-          <h2>That allows you to collect Tuition Fees online</h2>
-          <h2>Student's Pay using MTN Mobile Money</h2>
-          <img src={banner} alt="banner"/>
-        </section>
-
-        <section>
-          <h1>FEATURES</h1>
-          <h3> Student payments</h3>
-          <p>Students Can Make payments using MTN mobile money</p>
-          <img src={PayGif} alt="student payment gif"/>
-          <h3> Admin add Payment</h3>
-          <p>Site administrators can also add payments via the backend</p>
-          <img src={AdmGif} alt="admin payment gif"/>
-          <h3> Student History</h3>
-          <p>Students can view a history of their transactions</p>
-          <img src={HisImg} alt="student history"/>
+        <div className="container">
+         <div className="jumbotron text-center">
+              <h2>A Student Fees Mangement System</h2>
+              <h5>Allows Schools collect Tuition Fees online</h5>
+              <h5>Students and Parents Pay using Mobile Money</h5>    
+          </div>
+          <div>
+                <img className="embed-responsive-item" src={banner} alt="banner"/> 
+          </div>
+          <h3>Benefits</h3>
+          <div className="row">
+                    <div className="col-sm-4">
+                        <ul>
+                          <li>Saves Time</li>
+                          <li>More Efficient</li>
+                          <li>Secure and Reliable</li>
+                          <li>Low Risk of Theft</li>
+                          <li>Eliminate Carbon Footprint</li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-4"></div>
+                    <div className="col-sm-4">
+                        <ul>
+                          <li>Reduces Labour</li>
+                          <li>Quick and Easy Setup</li>
+                          <li>Decreases Late Payments</li>
+                          <li>Immediate Confirmation</li>
+                          <li>Convenient Storage of Data</li>
+                        </ul>
+                    </div>
+              </div>
+        </div>
+          <div className="container">
+              <div className="jumbotron text-center">
+                <h1 id="features">FEATURES</h1>
+                <h3> Student payments</h3>
+                <p>Students Can Make payments using MTN mobile money</p>
+                <img src={PayGif} alt="student payment gif"/>
+                <br/>
+                <br/>
+                <br/>
+                <h3> Admin add Payment</h3>
+                <p>Site administrators can also add payments via the backend</p>
+                <img src={AdmGif} alt="admin payment gif"/>
+                <br/>
+                <br/>
+                <br/>
+                <h3> Student History</h3>
+                <p>Students can view a history of their transactions</p>
+                <img src={HisImg} alt="student history"/>
+              </div>
+          </div>
           
-        </section>
-
-        <section>
-          <h1>ABOUT</h1>
-          <p>This is a portfolio project for <a target="blank" href="https://www.holbertonschool.com/">Holberton School</a></p>
-          The project was inspired by the need to deconjest banks during back to school periods,
-          reduce manaul work for school finance departments, and improve peoples productivity overall.
-
-          <h4>Developer: Peter Sangwani Zyambo</h4>
-          <a href="#">LinkedIn</a> 
-          <a href="#">Twitter</a> 
-          <a href="#">Github</a> 
-
-          <h3>Project Repository</h3>
-          <a href="#">Github</a> 
-        </section>
+          <div className="container">
+            <div className="jumbotron text-center">
+              <h2 id="about">About The Project</h2>
+              <p>This is a portfolio project for <a target="blank" href="https://www.holbertonschool.com/">Holberton School</a></p>
+              <p>The project was inspired by the need to deconjest banks during back to school periods,</p>
+              <p>reduce manaul labour for schools and save time and risk of money loss and thefts by students</p>
+              <br/>
+              <br/>
+              <h4 id="bio"> Developer Bio</h4>
+              My name is Peter Sangwani Zyambo and I am a full-stack Web Application Developer and Software Engineer, currently living in Kitwe, Copperbelt, Zambia. 
+              My primary focus and inspiration for my studies is Web Development. In my free time, I study human computer interface and the psychology of human computer interaction.
+              I am both driven and self-motivated, and I am constantly experimenting with new technologies and techniques. I am very passionate about software development,
+              and strive to better myself as a developer, and the African development community as a whole mostly here in my country Zambia.
+              <br/>
+              <br/>
+              <h4> Languages</h4>
+              I develop in C, C++, Python and Java though I primarily use Python. I develop both the back and front-end, 
+              and I am proficient in HTML/HTML5, CSS, JavaScript, React, jQuery and SQL/MySQL/PostgreSQL, to name a few. 
+              I have worked on desktop, web and mobile applications.
+              <br/>
+              <br/>
+              <div className="row" id="contact">
+                    <div className="col-sm-4">
+                        LinkedIn
+                    </div>
+                    <div className="col-sm-4">
+                        Twitter
+                    </div>
+                    <div className="col-sm-4">
+                       Github
+                    </div>
+              </div>
+              <br/>
+              <br/>
+              <h4> Project Repo</h4>
+              <br/>
+                Github
+             
+            </div>
+          </div>
     </div>
   );
 };
--- sfms_frontend/frontend/src/components/Layout.js
+++ sfms_frontend/frontend/src/components/Layout.js
@@ -1,6 +1,7 @@
 import { Outlet, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, {useState, useEffect, Fragment} from "react";
+import logo from "../assets/d.png";
 
 const Layout = () => {
 
@@ -13,9 +14,12 @@ const Layout = () => {
   return (
       <>
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link to={'/'} className="nav-link active"  aria-current="page" >SkoolPay</Link>
+  <a href="#" class="navbar-brand">
+            <img src={logo} height="28" alt="logo"/>
+    </a>
+  <Link to={'/'} className="nav-link active"  aria-current="page" ><h1>SkoolPay</h1></Link>
 {/* <h1 className="nav-link active" aria-current="page" >SkoolPay</h1> */}
-<div>
+<div className="container">
   <ul className="nav justify-content-end">
     {isAuth === true ? (
       <Fragment>
@@ -30,7 +34,13 @@ const Layout = () => {
       <Link  className="nav-link" to="/history">HISTORY /</Link>
     </li>
     <li className="nav-item">
-      <Link className="nav-link" to="/logout">LOG OUT</Link>
+      <a className="nav-link" href="#features">FEATURES /</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#about">ABOUT /</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#bio">BIO</a>
     </li>
       </Fragment>
     ) : (
