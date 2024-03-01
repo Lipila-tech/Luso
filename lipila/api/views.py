9b06e8e4af0fec4f123deab8f3a00b3e15b51d8e
sangwani <mightypz2@gmail.com>
2024-03-01 20:02:02 +0200

    Test: Write unittest for the SignupView

--- lipila/api/views.py
+++ lipila/api/views.py
@@ -2,7 +2,6 @@ import environ
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
 
 from rest_framework import status
 from rest_framework import views, viewsets
--- lipila/web/tests.py
+++ lipila/web/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from web.forms.forms import SignupForm
+from django.contrib.messages import get_messages
 
-# Create your tests here.
+from api.models import MyUser
+
+
+# TEST SignupView
+class SignupViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('signup')
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], SignupForm)
+        self.assertTemplateUsed(response, 'Auth/signup.html')
+
+    def test_post_valid_form(self):
+        data = {
+            "username": "testuser",
+            "phone_number": "2244556677",
+            "password": "test@123",
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(messages[0].level, 25)
+        self.assertEqual(messages[0].message, 'Account created successfully')
+
+        # check that a user with a specified username is created
+        # but not active
+        user = MyUser.objects.get(username='testuser')
+        self.assertTrue(user.is_active)
+
+    def test_post_invalid_form(self):
+        data = {
+            "username": "####",
+            "email": "testemail@bot.com",
+            "password": "test@123"
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], SignupForm)
+        self.assertTemplateUsed(response, 'Auth/signup.html')
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(messages[0].level, 40)
+        self.assertEqual(messages[0].message, 'Error during signup!')
