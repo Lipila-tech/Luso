862d09f16f92cf2ce8147bf38aa36411f5d58afa
sangwani <mightypz2@gmail.com>
2024-03-01 19:34:11 +0200

    Chore: Display success message or error after signup

--- lipila/api/views.py
+++ lipila/api/views.py
@@ -8,7 +8,6 @@ from rest_framework import status
 from rest_framework import views, viewsets
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 
