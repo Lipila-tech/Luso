52cef3d9deb295b5d87048ceeb775a6d27d4ddac
sangwani <mightypz2@gmail.com>
2024-04-01 21:46:38 +0200

    Add Send Email implementation to production settings

--- lipila/backend/settings.py
+++ lipila/backend/settings.py
@@ -163,3 +163,12 @@ STATIC_URL = "static/"
 
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "index"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_ID') 
+EMAIL_HOST_PASSWORD = env('EMAIL_PW')
+
+DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
