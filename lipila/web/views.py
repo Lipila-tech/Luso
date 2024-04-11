f0ad5502cec37a353159703ea5119c84b4843540
sangwani <mightypz2@gmail.com>
2024-04-11 22:24:08 +0200

    Implement custom home page for creators

--- lipila/web/views.py
+++ lipila/web/views.py
@@ -34,12 +34,11 @@ def user_profile(request, username):
         # username = request.GET.get('username')
         # Logic to retrieve user data based on username (e.g., from database)
         user_data = LipilaUser.objects.get(username=username)
-        context = {'user': user_data}
+        context['user'] = user_data
     except LipilaUser.DoesNotExist:
         context['status'] = 404
         context['message'] = f'{username} Not Found!'
         return apology(request, context, user=username)
-
     return render(request, 'profile/home.html', context)
 
 
