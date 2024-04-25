ab71e6db23eb454f4cde63d251384b774dfa6173
sangwani <mightypz2@gmail.com>
2024-04-25 21:06:55 +0200

    define MEDIA_URL and MEDIA_ROOT

--- lipila/backend/settings.py
+++ lipila/backend/settings.py
@@ -172,6 +172,9 @@ REST_FRAMEWORK = {
     ),
 }
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
