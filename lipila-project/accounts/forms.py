64559e6c7dc7a5293be862c6b3e95c1923da0569
sangwani <mightypz2@gmail.com>
2024-06-05 13:57:32 +0200

    [Refactor] remove help_text

--- lipila-project/accounts/forms.py
+++ lipila-project/accounts/forms.py
@@ -5,6 +5,15 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=150, help_text='Email')
 
+    def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.fields['password1'].help_text="Your password can't be too similar to your username."
+      self.fields['password2'].help_text='Confirm password.'
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+        help_texts = {
+            'username': None,
+            'email': None,
+        }
