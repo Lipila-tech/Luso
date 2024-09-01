ff2d7b7115d114d872a3b9e84504da5d4c976a03
sangwani <mightypz2@gmail.com>
2024-09-01 10:29:50 +0200

    [Fix currupted git]

--- README.md
+++ README.md
@@ -1,14 +1,83 @@
-# Luso
+# Lipila
 
-## Overview
+**Overview**
 
-Luso is the best way for Zambian creators and artists to accept support and membership from their fans. A free, fast, and beautiful way to fund their creative work.
+- Allows remote collection of fees via mobile money
+- Allows disbursement of funds to multiple users
 
-Key features include:
+## Local machine usage
+Ensure you have python 3 installed
+[Download it here](https://www.python.org/downloads/)
 
-- **Patron profile creation:** Create and customize profiles.
-- **Mobile money Integration:** Supports MTN and Airtel mobile payments.
-- **Creator search:** Fans can look up creators.
-- **Exclusive content sharing:** Creators can share exclusive content direct with fans.
-- **Tier customization**: Creators can customize the default tiers.
-- **One-Time Contributions:** Patrons can make a one time contribution.
+**Fork this repo then:**
+
+
+    git clone git@github.com:your-username/lipila-dot-tech.git
+
+    cd lipila-dot-tech    
+
+    python -m .venv
+
+    source .venv/bin/activate <!-- Linux -->
+
+    .venv/Scripts/activate <!-- Windows -->
+
+    pip install -r requirements.txt
+    
+
+    cd lipila
+
+    <!-- *Copy and set your environment variables in the .env file.* -->
+
+    cp backend/example.env backend/.env
+
+    python manage.py migrate
+    python manage.py runserver
+
+Access your Web App at:
+
+localhost:8000/
+
+[more](./lipila/business/)
+
+
+Access the Django Rest API Root at:
+
+localhost:8000/api/v1/
+
+[more](./lipila/api/)
+
+
+**API Keys COnfiguration**
+
+- Register to use the MTN momo api at
+    
+    https://momodeveloper.mtn.com
+
+Once you subscribe to a product copy the keys and add in your .env file.
+
+
+**Testing**
+
+api Code:
+    python -m manage.py test api.tests
+
+business app Code:
+    python -m manage.py test api.tests
+
+
+**Contributing**
+
+1. Fork the repository[lipila-dot-tech](https://github.com/Lipila-tech/Lipila-rest-api)
+2. Create a new branch for your changes. [see branching](https://git-scm.com/book/en/v2/Git-Branching-Branches-in-a-Nutshell)
+3. Make changes and commit them.
+4. Submit a pull request.[See making a pull request](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request)
+-
+
+**License**
+
+-
+
+**Contact**
+
+zyambopeter1@gmail.com
---
+++ accounts/views.py
@@ -0,0 +1,179 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import reverse
+
+#  custom modules
+from .utils import basic_auth_encode, basic_auth_decode
+from .forms import SignUpForm
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from django.conf import settings
+
+import random
+import string
+
+TIKTOK_CLIENT_KEY = settings.TIKTOK_CLIENT_KEY
+SERVER_ENDPOINT_REDIRECT=settings.SERVER_ENDPOINT_REDIRECT
+
+def oauth(request):
+    # Generate a random CSRF token
+    csrf_state = ''.join(random.choices(string.ascii_lowercase + string.digits, k=16))
+
+    # Set the CSRF token as a cookie
+    response = HttpResponse()
+    response.set_cookie('csrfState', csrf_state, max_age=60)
+
+    # Build the TikTok authorization URL
+    url = 'https://www.tiktok.com/v2/auth/authorize/'
+    url += f'?client_key={TIKTOK_CLIENT_KEY}'
+    url += '&scope=user.info.basic'
+    url += '&response_type=code'
+    url += f'&redirect_uri={SERVER_ENDPOINT_REDIRECT}'
+    url += f'&state={csrf_state}'
+
+
+    # Redirect to the TikTok authorization URL
+    return redirect(url)
+
+
+def sign_in(request):
+    return render(request, 'registration/signin.html')
+
+
+@csrf_exempt
+def auth_receiver(request):
+    """
+    Google calls this URL after the user has signed in with their Google account.
+    """
+    token = request.POST.get('credential')
+
+    if not token:
+        return HttpResponse(status=400)  # Bad request if no token is provided
+
+    try:
+        user_data = id_token.verify_oauth2_token(
+            token, requests.Request(), settings.GOOGLE_OAUTH_CLIENT_ID)
+    except ValueError:
+        return HttpResponse(status=403)  # Forbidden if token is invalid
+
+    email = user_data.get('email')
+
+    if not email:
+        return HttpResponse(status=400)  # Bad request if no email in token
+
+    # Get or create the user
+    user, created = get_user_model().objects.get_or_create(email=email, defaults={
+        'username': email.split('@')[0],  # You can modify this as needed
+        'first_name': user_data.get('given_name', ''),
+        'last_name': user_data.get('family_name', ''),
+    })
+
+    if created:
+        messages.success(request, "Account created.")
+        user.set_unusable_password()  # Set unusable password if creating a new user
+        user.save()
+
+    # Authenticate and log in the user
+    user = authenticate(request, email=email)
+    if user is not None:
+        messages.success(request, f"Welcome back, {user.username}!")
+        login(request, user)
+        request.session['user_data'] = user_data
+        return redirect(reverse('dashboard'))
+    else:
+        messages.error(request, "Authentication failed")
+        return redirect(reverse('accounts:signup'))
+
+
+def sign_out(request):
+    del request.session['user_data']
+    return redirect('accounts:signin')
+
+
+def activation_sent_view(request):
+    return render(request, 'registration/activation_sent.html')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = basic_auth_decode(uidb64)
+        user = get_user_model().objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        # Default backend or specific one
+        backend = settings.AUTHENTICATION_BACKENDS[0]
+        login(request, user, backend=backend)
+        return redirect('patron:create_creator_profile')
+    else:
+        return render(request, 'registration/activation_invalid.html')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Please Activate Your Account'
+            uid64 = basic_auth_encode(user.pk)
+            token = default_token_generator.make_token(user)
+            message = render_to_string('registration/activation_request.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': uid64,
+                'token': token,
+            })
+            user.email_user(subject, message)
+            return redirect('accounts:activation_sent')
+        else:
+            messages.error(request, "Invalid form fields.")
+            form = SignUpForm()
+            return render(request, 'registration/signup.html', {'form': form})
+
+    form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def custom_login_view(request):
+    if request.method == 'POST':
+        next_url = request.GET.get('next', 'dashboard')
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if user.has_group or user.is_staff:
+                    messages.success(request, f"Welcome back, {user.username}!")
+                    return redirect(next_url)
+                else:
+                    return redirect('patron:create_creator_profile')
+            else:
+                form = AuthenticationForm()
+                return render(request, 'registration/login.html', {'form': form})
+        else:
+            messages.error(request, "Invalid username or password.")
+            form = AuthenticationForm()
+            return render(request, 'registration/login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'registration/login.html', {'form': form})
