5635c89e1a6a7424eed18c11c7799926de12517b
sangwani <mightypz2@gmail.com>
2024-10-03 13:56:49 +0200

    add error msg

--- accounts/views.py
+++ accounts/views.py
@@ -174,19 +174,18 @@ def tiktok_callback(request):
             # User already exists, retrieve the user
             user = social_auth.user
             # Authenticate and log in the user
-            messages.success(request, f"Welcome back, {tiktok_username}!")
-
-        user = authenticate(request, username=tiktok_username)
-        if user is not None:
-            login(request, user,
-                    backend='accounts.auth_backends.EmailOrUsernameModelBackend')
-            request.session['tiktok_user_data'] = access_token
-            return redirect(reverse('dashboard'))
-        else:
-            messages.error(request, "Authentication failed")
-            return redirect(reverse('accounts:signup'))
+            user = authenticate(request, username=tiktok_username)
+            if user is not None:
+                messages.success(request, f"Welcome back, {tiktok_username}!")
+                login(request, user,
+                        backend='accounts.auth_backends.EmailOrUsernameModelBackend')
+                request.session['tiktok_user_data'] = access_token
+                return redirect(reverse('dashboard'))
+            else:
+                messages.error(request, "Authentication failed")
+                return redirect(reverse('accounts:signup'))
     else:
-        messages.error(request, "Authentication failed")
+        messages.error(request, "Tiktok Authentication failed")
         return redirect(reverse('accounts:signup'))
 
 
