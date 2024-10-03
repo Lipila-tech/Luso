b48b2f3d0547dc5f534aa1eb73734b9f71739917
sangwani <mightypz2@gmail.com>
2024-10-03 11:58:58 +0200

    use passwordInput widget

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,14 +2,17 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
+
 class SignUpForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)  # Use PasswordInput widget
+
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'password')
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password'])
+        user.set_password(self.cleaned_data['password'])  # Hash the password
         if commit:
             user.save()
         return user
